3ec64291962a8100cc23d97bfec768c8ee8e8eeb
schurchleycci <82465475+schurchleycci@users.noreply.github.com>
2023-04-11 11:28:31 -0400

    Update cartItemCard.spec.ts

--- tests/unit/cartItemCard.spec.ts
+++ tests/unit/cartItemCard.spec.ts
@@ -5,15 +5,15 @@ describe("CartItemCard.vue", () => {
   it("renders the menuItem prop data", () => {
     const menuItem = {
       id: 1,
-      name: "Sweet Potato Fries",
+      name: "French Fries",
       description: "Fried potato rectangles",
-      price: "2.99",
+      price: "4.99",
       imageId: "1",
     };
     const wrapper = mount(CartItemCard, {
       props: { menuItem },
     } as any);
 
-    expect(wrapper.text()).toContain("French");
+    expect(wrapper.text()).toContain("4.99");
   });
 });
